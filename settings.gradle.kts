6e83f7d9f65500a1c3a90cd9a7ca08e134c1f9e8
brosssh <tiabroch@gmail.com>
2025-11-28 16:39:45 +0100

    feat: Use patcher v22 by Liso

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,6 +18,20 @@ dependencyResolutionManagement {
         }
         maven {
             // A repository must be specified for some reason. "registry" is a dummy.
+            url = uri("https://maven.pkg.github.com/brosssh/registry")
+            credentials {
+                val hardcodedUser = ""
+                val hardcodedToken = ""
+                val gprUser: String? = providers.gradleProperty("gpr.user").orNull
+                val gprKey: String? = providers.gradleProperty("gpr.key").orNull
+
+                username = (if (hardcodedUser.isNotBlank()) hardcodedUser else System.getenv("GITHUB_ACTOR") ?: gprUser)
+
+                password = (if (hardcodedToken.isNotBlank()) hardcodedToken else System.getenv("GITHUB_TOKEN") ?: gprKey)
+            }
+        }
+        maven {
+            // A repository must be specified for some reason. "registry" is a dummy.
             url = uri("https://maven.pkg.github.com/revanced/registry")
             credentials {
                 val hardcodedUser = ""
