0b63e83cfa9322444c4a16a27e8c9d32d6b89e82
Jman-Github <128645077+Jman-Github@users.noreply.github.com>
2025-12-29 16:43:04 -0500

    ci: fix dev build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -151,7 +151,7 @@ android {
         minSdk = 26
         targetSdk = 35
 
-        val versionStr = if (version == "unspecified") "1.7.0" else version.toString()
+        val versionStr = if (version == "unspecified") "1.6.1" else version.toString()
         versionName = versionStr
         versionCode = with(versionStr.toVersion()) {
             major * 10_000_000 +
@@ -186,6 +186,11 @@ android {
             initWith(getByName("release"))
             versionNameSuffix = "-dev"
             signingConfig = releaseSigningConfig
+            if (!project.hasProperty("noProguard")) {
+                isMinifyEnabled = true
+                isShrinkResources = true
+                proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            }
             buildConfigField("long", "BUILD_ID", "${Random.nextLong()}L")
         }
 
