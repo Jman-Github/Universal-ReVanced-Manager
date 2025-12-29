a8190f20b908a286489b47209b5d629a2f43fd3c
Jman-Github <128645077+Jman-Github@users.noreply.github.com>
2025-12-29 16:01:05 -0500

    ci: replace debug builds in `Dev Builds` workflow with dev builds

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -162,9 +162,30 @@ android {
         vectorDrawables.useSupportLibrary = true
     }
 
+    val keystoreFile = file("keystore.jks")
+    val releaseSigningConfig = if (project.hasProperty("signAsDebug") || !keystoreFile.exists()) {
+        signingConfigs.getByName("debug")
+    } else {
+        signingConfigs.create("release") {
+            storeFile = keystoreFile
+            storePassword = System.getenv("KEYSTORE_PASSWORD")
+            keyAlias = System.getenv("KEYSTORE_ENTRY_ALIAS")
+            keyPassword = System.getenv("KEYSTORE_ENTRY_PASSWORD")
+        }
+    }
+
     buildTypes {
         debug {
             isPseudoLocalesEnabled = true
+            versionNameSuffix = "-dev"
+            signingConfig = releaseSigningConfig
+            buildConfigField("long", "BUILD_ID", "${Random.nextLong()}L")
+        }
+
+        create("dev") {
+            initWith(getByName("release"))
+            versionNameSuffix = "-dev"
+            signingConfig = releaseSigningConfig
             buildConfigField("long", "BUILD_ID", "${Random.nextLong()}L")
         }
 
@@ -175,19 +196,7 @@ android {
                 proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
             }
 
-            val keystoreFile = file("keystore.jks")
-
-            signingConfig = if (project.hasProperty("signAsDebug") || !keystoreFile.exists()) {
-                signingConfigs.getByName("debug")
-            } else {
-                signingConfigs.create("release") {
-                    storeFile = keystoreFile
-                    storePassword = System.getenv("KEYSTORE_PASSWORD")
-                    keyAlias = System.getenv("KEYSTORE_ENTRY_ALIAS")
-                    keyPassword = System.getenv("KEYSTORE_ENTRY_PASSWORD")
-                }
-            }
-
+            signingConfig = releaseSigningConfig
             buildConfigField("long", "BUILD_ID", "0L")
         }
     }
